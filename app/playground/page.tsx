5c6f6d97c6e95715265a92e065da3fccf8262b53
Simon-Stone29 <so29ston@siena.edu>
2026-08-01 14:36:12 -0400

    added new turnaround components to playground page.tsx

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -3,32 +3,32 @@
 import Script from "next/script";
 
 import BatmanTurnaround from "@/components/BatmanTurnaround";
-
+import LegoTurnaround from "@/components/LegoTurnaround";
+import ArtTurnaround from "@/components/ArtTurnaround";
 export default function Playground() {
   return (
     <div className="min-h-screen flex flex-col items-center px-6 py-16 gap-16">
       <div className="max-w-2xl">
         <h1 className="text-4xl font-bold mb-8">Playground</h1>
         <p className="text-lg text-gray-600 leading-relaxed mb-8">
-          Aside from coding, I like the creative aspects of 3D modeling in
+          Aside from coding, I like the creative aspects of 3D modeling, animation, and rendering in
           softwares like Blender. Here is some of my work:
         </p>
       </div>
 
+      <h3>Batman Animated Sculpt</h3>
       <div className="w-full max-w-2xl bg-black rounded-xl p-8">
         <BatmanTurnaround />
       </div>
-
-      <div className="max-w-2xl w-full">
-        <model-viewer
-          src="/models/siena.glb"
-          alt="Siena Hall 3D model"
-          camera-controls
-          auto-rotate
-          style={{ width: "100%", height: "600px" }}
-        />
+      <h3>Siena Hall Lego Model</h3>
+            <div className="w-full max-w-2xl bg-black rounded-xl p-8">
+        <LegoTurnaround/>
       </div>
-      <div className="max-w-2xl w-full">
+      <h3>Art the Clown Sculpt</h3>
+            <div className="w-full max-w-2xl bg-black rounded-xl p-8">
+        <ArtTurnaround/>
+      </div>
+{/*             <div className="max-w-2xl w-full">
         <model-viewer
           src="/models/Batman3dPrint.glb"
           alt="Batman 3D model"
@@ -36,7 +36,20 @@ export default function Playground() {
           auto-rotate
           style={{ width: "100%", height: "600px" }}
         />
+      </div> */}
+      <h3>Dinosaur VFX Breakdown</h3>
+      <div className="w-full max-w-2xl bg-black rounded-xl overflow-hidden shadow-xl border border-neutral-800">
+        <video
+          className="w-full h-auto cursor-pointer"
+          controls
+          preload="metadata"
+          playsInline
+        >
+          <source src="/DinosaurBreakdown.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
+    
       <Script
         type="module"
         src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js"
