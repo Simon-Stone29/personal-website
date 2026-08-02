9662fa80707601be7c43139cb3c2bf774e139f06
Simon-Stone29 <so29ston@siena.edu>
2026-08-02 18:42:58 -0400

    removed bottom glass blur effect

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({
       <body>
         <Navigation />
         <main className="pt-16">{children}</main>
-        <div className="bottom-blur" />
       </body>
     </html>
   );
